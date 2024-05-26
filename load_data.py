a50b25a246c70d47d6e8a5e966af2fbe9431fd8a
1970Mr <mrepoch1970@gmail.com>
2024-05-26 14:28:39 +0430

    Refactor the load_data.py and add some options to it

--- load_data.py
+++ load_data.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 from sqlalchemy import inspect
 from dotenv import load_dotenv
 from app import create_app, db, bcrypt
@@ -11,7 +12,15 @@ load_dotenv()
 app = create_app()
 app.app_context().push()
 
-def import_questions(json_file):
+
+def import_questions(json_file, add_only):
+    if not add_only:
+        # Clear existing questions
+        db.session.query(AnsweredQuestion).delete()
+        db.session.query(Question).delete()
+        db.session.query(GameData).delete()
+        db.session.commit()
+
     with open(json_file, 'r') as file:
         questions = json.load(file)
 
@@ -28,9 +37,11 @@ def import_questions(json_file):
 
     db.session.commit()
 
+
 def table_exists(table_name):
     inspector = inspect(db.engine)
     return inspector.has_table(table_name)
+
 
 def create_admin():
     admin_username = os.getenv('ADMIN_USERNAME')
@@ -49,28 +60,42 @@ def create_admin():
         else:
             print("Admin user already exists.")
 
+
 if __name__ == "__main__":
     app = create_app()
     with app.app_context():
+        if len(sys.argv) < 2:
+            print("Usage: python script.py <path_to_json_file> [--add-only]")
+            sys.exit(1)
+
+        json_file_path = sys.argv[1]
+        add_only = '--add-only' in sys.argv
+
+        if not os.path.exists(json_file_path):
+            print(f"File {json_file_path} does not exist.")
+            sys.exit(1)
+
         tables = db.metadata.tables.keys()
 
         # Check if the tables exist
         if not all(table_exists(table) for table in tables):
             db.create_all()
-            import_questions('data/questions.json')
+            import_questions(json_file_path, add_only)
             create_admin()
             print("All tables created, questions imported, and admin user created successfully!")
         else:
-            # Drop and recreate only the non-user tables
-            if table_exists('question'):
-                Question.__table__.drop(db.engine)
-            if table_exists('game_data'):
-                GameData.__table__.drop(db.engine)
-            if table_exists('answered_question'):
-                AnsweredQuestion.__table__.drop(db.engine)
+            if not add_only:
+                # Drop and recreate only the non-user tables
+                if table_exists('question'):
+                    Question.__table__.drop(db.engine)
+                if table_exists('game_data'):
+                    GameData.__table__.drop(db.engine)
+                if table_exists('answered_question'):
+                    AnsweredQuestion.__table__.drop(db.engine)
 
-            Question.__table__.create(db.engine)
-            GameData.__table__.create(db.engine)
-            AnsweredQuestion.__table__.create(db.engine)
-            import_questions('data/questions.json')
+                Question.__table__.create(db.engine)
+                GameData.__table__.create(db.engine)
+                AnsweredQuestion.__table__.create(db.engine)
+
+            import_questions(json_file_path, add_only)
             print("Questions imported successfully!")
